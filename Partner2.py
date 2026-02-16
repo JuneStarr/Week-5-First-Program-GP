7f52f42a0031400a0175ab800fb6d3af425ed443
JuneStarr <garielpolanco9@gmail.com>
2026-02-16 18:16:41 +0000

    Assignment
    Co-authored-by: Bwilson01123 <Bwilson01123@users.noreply.github.com>

---
+++ Partner2.py
@@ -0,0 +1,39 @@
+"""
+P1: Brian Wilson
+P2: Hold on chat gpt taking long to answer
+"""
+
+
+#this is a comment i think (i hope)
+#67 67 67 67 67 67 67 67
+#Start of an amzing program
+#uhhhh this will be the best AI ever
+#the name will be UTRGV-AI
+#will be better than chat gpt
+#@grok is this true?
+#grok: idk bruh
+
+print('User: @grok is this code good?!!1!???')
+
+print("@grok: yo")
+print("@grok: oh yeah type shi")
+
+if True: 
+    print("yeah its true or wtv") #meaning its true or something like that
+
+i=True
+while i == 1:
+    print ("Worst possible if statment")
+    i = i-1
+
+""""
+welcome to my string literal that is a
+comment by function
+
+interp knows that it is a string,
+but since it doesnt have a variable,
+interp does not care lol
+
+type here if you have ever been ignored dispite existing:
+ 
+"""
