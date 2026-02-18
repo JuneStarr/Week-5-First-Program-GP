28dc9f1b9345a466acd536696c2b45ccd75a4c83
JuneStarr <garielpolanco9@gmail.com>
2026-02-18 17:53:23 +0000

    lil assignment
    Co-authored-by: Bwilson01123 <Bwilson01123@users.noreply.github.com>

---
+++ inclass .py
@@ -0,0 +1,21 @@
+name = "Gariel"
+number = 67
+
+print ("Yo wsg im about to print something watch out")
+
+print("Is your name ", name)
+print("that shit sucks, wouldnt wish it on my worst enemy")
+print("fuck you")
+
+print("ok who's your favorite artist?")
+artist = input()
+print("Ive never heard of that guy pfft")
+print(artist,",whats that type of name!?!")
+
+x = 67
+y = 41
+
+if x > y:
+    print("67 is better than 41")
+else:
+     print("67 is unfortunately smaller than 41")
---
+++ partner2-2-18.py
@@ -0,0 +1,41 @@
+'''
+p2:Gariel Polanco
+p1:Brian Wilson
+'''
+
+# 1. Print a message to the screen
+print("Yo wsg im the compiler")
+# 2. Change the message to your own words and run again
+print("nevermind im not the compiler, im actually the programmer..")
+# 3. Print your favorite food
+print ("Favorite food is anything edible, food is food ")
+# 4. Try printing a number instead of text
+print (67)
+# 5. Create a variable that stores your name
+name = "Gariel"
+# 6. Print the variable
+print (name)
+# 7. Change the name inside the variable and run again
+name = "BabyBoo"
+print (name)
+# 8. Create a variable with a number
+age=67
+# 9. Print the age
+print("your age is", age, "haha 67...")
+# 10. Add 2 to the age and print the result
+age = age+2
+print (age)
+# 11. Ask the user for their name
+newname = input("whats your name sweetheart? ")
+# 12. Print a greeting using their name
+print ("Hello "+ newname )
+# 13. Ask the user for a number
+favNum= input ("What is your favorite number?")
+# 14. Print the number they typed
+print ("You chose "+str(favNum)+" as your number" )
+# 15. Print a goodbye message
+print("i freaking hate you", newname,"have a horrible christmas")
+
+'''
+no documentation cause we are them (both know python)
+'''
